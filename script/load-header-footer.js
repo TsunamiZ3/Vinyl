9796e3b085ae82032ac5d3f3fba7d362cbc02c60
Francesco Napoli <ftsuna@outlook.it>
2021-04-04 00:09:02 +0200

    Update changelog: improved buttons animation, removed footer, removed ajax page loading script.

--- script/load-header-footer.js
+++ script/load-header-footer.js
@@ -5,11 +5,3 @@ fetch("../default/header.html")
   .then(data => {
     document.querySelector("header").innerHTML = data;
 });
-
-fetch("../default/footer.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("footer").innerHTML = data;
-});
